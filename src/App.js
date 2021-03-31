cd0c3180c263878b77f548fbd08fb92b7a5da2be
ID-DEM <ouou123asp123@gmail.com>
2021-03-31 13:14:07 +0900

    implement-object-state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import React from "react"
 import {useState} from "react"
 
-const App =  (props)=> {
+const App = (props)=> {
 
-
-  const [name,setName] = useState(props.name)
-  const [price,setPrice] = useState(props.price)
-
-  const reset = () =>{
-    setPrice(props.price)
-    setName(props.name)
-  }
+  // こっちは useState の展開方法
+  const [state,setState] = useState(props)  // props をobjとして直接渡せる
+ 
+  // こっちは分割代入
+  const {name,price} = state
+  
 
 return (
 <>
 <p>現在の{name}は、{price}円です</p>
-<button onClick={()=>setPrice(price + 1)}>+1</button>
+<button onClick={()=>setState({...state,price:price + 1})}>+1</button>
 <br/>
-<button onClick={()=>setPrice(price - 1)}>-1</button>
-<br/>
-
-<button onClick={reset}>Reset</button>
+<button onClick={()=>setState({...state,price:price - 1})}>-1</button>
 <br/>
 
-<input type="text" value={name}  onChange={ (e) => setName(e.target.value) }/>
+<button onClick={ () => setState(props)}>Reset</button>
+<br/>
+
+<input type="text" value={name}  onChange={ (e) => setState({...state,name:e.target.value}) }/>
      </>
   );
 }
